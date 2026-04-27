1801df0e638a74d1e96fbe9080b8547c19fcbe3f
lalaya21 <lalaya.shuckahosee@students.msswarriors.org>
2026-04-27 19:17:35 +0000

    done3

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ function calculateAward(points) {
     if (points > 99) {
         return "First Place";
     } else if (points > 49 && points < 100) {
-    return "Second Place"
-}else {
-    return " participation Award";
-}
+    return "Second Place";
+} else {
+    return "Participation Award";
+    }
 }
